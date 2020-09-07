adc301aa1bb4d239794f39bf87b8719323e78ace
cleavy <1667037210@qq.com>
2020-09-07 18:39:22 +0800

    电影院选票系统 初版

--- select_tickets/select_main.py
+++ select_tickets/select_main.py
@@ -1,14 +1,5 @@
 from select_tickets import select_tools
 
-print('荧幕'.center(45))
-print('')
-print('')
-results = range(1, 101)
-for i in range(len(results)):
-    print(str(results[i]).rjust(2, '0').center(4), end=',')
-    if results[i] % 10 == 0:
-        print('\n')
-
 while True:
 
     # 展示电影院的所有座位
@@ -17,20 +8,24 @@ while True:
     action = int(input('请选择您的操作：'))
 
     # 查看座位号 1
-    if action in range(4):
-        if action == 1:
-            select_tools.select_tickes()
-    # 选择座位号 2
-        elif action == 2:
-            select_tools.show_tickets()
-    # 退票 3
-        elif action == 3:
-            select_tools.checkout_tickets(select_tools.select_tickes())
-
-
+    # if action in range(4):
+    if action == 1:
+        while True:
+            ticket_no = input('请选择您的座位号(1-100)：')
+            if select_tools.select_tickes(ticket_no) is not False:
+                break
+# 选择座位号 2
+    elif action == 2:
+        select_tools.show_tickets()
+# 退票 3
+    elif action == 3:
+        while True:
+            out_ticket = input('请输入您要申请的退票号：')
+            if select_tools.checkout_tickets(out_ticket) is not False:
+                break
     # 返回主页面 0
-        else:
-            print('欢迎您再次使用影院订票系统，祝您观影愉快！！！')
-            break
+    elif action == 0:
+        print('欢迎您再次使用影院订票系统，祝您观影愉快！！！')
+        break
     else:
         print('您输入有误，请重新输入')
--- select_tickets/select_tools.py
+++ select_tickets/select_tools.py
@@ -1,4 +1,5 @@
 
+
 # 展示主系统
 def show_mune():
     print('*' * 50)
@@ -10,49 +11,50 @@ def show_mune():
     print('0 是退出系统')
     print('*' * 50)
 
-
+tickets = [1,	2,	3,	4,	5,	6,	7,	8,	9,	10,
+           11,	12,	13,	14,	15,	16,	17,	18,	19,	20,
+           21,	22,	23,	24,	25,	26,	27,	28,	29,	30,
+           31,	32,	33,	34,	35,	36,	37,	38,	39,	40,
+           41,	42,	43,	44,	45,	46,	47,	48,	49,	50,
+           51,	52,	53,	54,	55,	56,	57,	58,	59,	60,
+           61,	62,	63,	64,	65,	66,	67,	68,	69,	70,
+           71,	72,	73,	74,	75,	76,	77,	78,	79,	80,
+           81,	82,	83,	84,	85,	86,	87,	88,	89,	90,
+           91,	92,	93,	94,	95,	96,	97,	98,	99,	100]
 # 显示电影票
+# 如果座位被占用，用**代替，其他正常输出
+# 查看座位的时候不显示已经被占用的座位号
+
 def show_tickets():
     print('荧幕'.center(45))
     print('')
-    results = range(1, 101)
-    for i in range(len(results)):
-        print(str(results[i]).rjust(2, '0').center(4), end=',')
-        if results[i] % 10 == 0:
-            print('\n')
-
-# 如果座位被占用，用**代替，其他正常输出
-
-
-# 查看座位的时候不显示已经被占用的座位号
-
+    for i in range(len(tickets)):
+        if (i + 1) % 10 == 0:
+            print(tickets[i], end="\n")
+        else:
+            print(tickets[i], end="\t")
 
 # 选择电影票
-def select_tickes():
-    while True:
-        ticket_no = input('请选择您的座位号(1-100)：')
-        if int(ticket_no) in range(1, 101):
-            if int(ticket_no) < 10:
-                print('您选择的作为号是【%s】' % ('0'+ticket_no))
-            else:
-                print('您选择的作为号是【%s】' % ticket_no)
-            return ticket_no
+def select_tickes(ticket_no):
+
+        if int(ticket_no) in range(1, 101) and tickets[int(ticket_no) - 1] != '**':  # tickets[int(ticket_no) - 1] 票号是1，角标是0
+            print('您选择的座位号是【%s】' % ticket_no)
+            tickets[int(ticket_no)-1] = '**'
+
         else:
             print('您选择的座位号有误，请重新选择！')
-
-
+            return False
 # 退票
+def checkout_tickets(out_ticket):
 
-def checkout_tickets(x):
-    while True:
-        t = input('请输入您要申请的退票号：')
-        if int(t) in range(1, 101) and t == x :
-            print(('您申请的退票号是：%s') % t)
+        if int(out_ticket) in range(1, 101) and tickets[int(out_ticket) - 1] == "**":
+            print(('您申请的退票号是：%s') % out_ticket)
             print('申请退票成功')
-            break
+            tickets[int(out_ticket) - 1] = out_ticket    # 将out_ticket 重新赋值给tickets[int(out_ticket) - 1]
+
         else:
             print('您申请的退票号不存在，请重新输入！')
-
+            return False
 
 
 
